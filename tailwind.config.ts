f97ce815a1debfc95101b122f404584129966a55
Kaliza Esther <kalizaesther5@gmail.com>
2026-07-14 20:15:28 +0200

    updating the ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,10 +15,16 @@ const config: Config = {
         neonPurple: "var(--neon-purple)",
         neonBlue: "var(--neon-blue)",
         neonCyan: "var(--neon-cyan)",
+        "cyber-neon": "#00ffcc",
+      },
+      animation: {
+        "spin-slow": "spin 8s linear infinite",
+      },
+      perspective: {
+        "1000": "1000px",
       },
     },
   },
   plugins: [],
 };
-
 export default config;
